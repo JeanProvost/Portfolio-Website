33e5765de005f8c469c3c628167cb95d1adefebc
JeanProvost <jprov310@gmail.com>
2025-04-24 21:06:04 -0600

    Updating Hero image and gradient

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,10 +4,8 @@ import styles from './Hero.module.css';
 const Hero: React.FC = () => {
   return (
     <section id="hero" className={styles.heroSection}>
-      <h1>Hero Section</h1>
-      <div className={styles.heroImageContainer}></div>
       <div className={styles.heroContent}>
-
+        <h1>Hero Section</h1>
       </div>
     </section>
   );
